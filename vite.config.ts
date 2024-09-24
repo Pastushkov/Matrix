4ba5c86801f86fd9122078f2e0319b736528fc1a
pastushkov <dimaspas62@gmail.com>
2024-09-24 10:10:12 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/Matrix",
   server: {
     strictPort: true,
     port: 3000,
